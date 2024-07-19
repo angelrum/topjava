390461c5bf6c266c41fe3588110ddc54e7f07211
vvolkov <angelrum@inbox.ru>
2024-07-19 23:57:24 +0300

    prepare_to_HW0_patch

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -2,7 +2,7 @@ package ru.javawebinar.topjava;
 
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo application</a>
- * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>ss
+ * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
     public static void main(String[] args) {
